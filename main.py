1d9ff9eee62bf96d01b8caa9e76a40c9cc5f2276
ben@blueinno <ben@blueinnotechnology.com>
2025-01-11 11:44:13 +0800

    l1 ans

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Q1: 
 '''
 
-x = None    # TODO: Replace `None` with your answer
+x = 10    # TODO: Replace `None` with your answer
 
 print('Q1:')
 print(f'x = {x}')
@@ -13,9 +13,9 @@ print('#' * 30)
 Q2:
 '''
 
-a = None    # TODO: Replace `None` with your answer
-b = None    # TODO: Replace `None` with your answer
-c = None    # TODO: Replace `None` with your answer
+a = 6    # TODO: Replace `None` with your answer
+b = 9    # TODO: Replace `None` with your answer
+c = 16    # TODO: Replace `None` with your answer
 
 print('Q2:')
 print(f'a = {a}')
@@ -27,8 +27,8 @@ print('#' * 30)
 Q3:
 '''
 
-first = None    # TODO: Replace `None` with your answer
-second = None   # TODO: Replace `None` with your answer
+first = 19    # TODO: Replace `None` with your answer
+second = 8   # TODO: Replace `None` with your answer
 
 print('Q3:')
 print(f'first = {first}')
@@ -40,19 +40,52 @@ Q4:
 '''
 
 # TODO: Write your code here
-
+first = 8
+second = 19
+first += second
+second = first - second
+first -= second
 ##################################################
 '''
 Q5:
 '''
 
 # TODO: Write your code here
+work_hours = 4 + 5 + 8 + 4
+salary = 8.75
+tax = 0.20
+# Calculate pay at work based on hours worked each day
+print("My total hours worked:")
+print(work_hours)
 
+print("My hourly salary:")
+print(f"${salary}")
+
+print("My total pay:")
+print((work_hours) * salary)
+
+print("My taxes owed:")  # 20% tax
+print((work_hours) * salary * tax)
 ##################################################
 '''
 Q6:
 '''
 
 # TODO: Write your code here
+# This program computes the total amount owed for a meal,
+# assuming 8% tax and a 15% tip.
+subtotal = 38 + 40 + 30
+tax_rate = 0.08
+tip_rate = 0.15
+print("Subtotal:")
+print(subtotal)
 
+print("Tax:")
+print(subtotal * tax_rate)
+
+print("Tip:")
+print(subtotal * tip_rate)
+
+print("Total:")
+print(subtotal * (1 + tax_rate + tip_rate))
 ##################################################
